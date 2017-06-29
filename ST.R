c79bacdec1bec43b6f64eed770d542265fa45ba1
smastelini <saulomastelini@gmail.com>
2017-06-29 17:31:36 -0300

    ST now using data.tables

--- ST.R
+++ ST.R
@@ -11,32 +11,37 @@ for(i in 1:length(bases)) {
 	targets[[i]] <- colnames(dataset)[(ncol(dataset)-n.targets[i]+1):ncol(dataset)]
 
 	dataset <- dataset[sample(nrow(dataset)),]
-	# sample.names <- dataset[,c(1)]
-	# dataset <- dataset[,-c(1)]
 	sample.names <- rownames(dataset)
 
-	dataset <- as.data.frame(sapply(dataset, function(x) as.numeric(x)))
+	#Center and Scaling
+	dataset <- as.data.table(dataset)
+	invisible(dataset[, names(dataset) := lapply(.SD, as.numeric)])
 
-	maxs[[i]] <- apply(dataset, 2, max)
-	mins[[i]] <- apply(dataset, 2, min)
-	dataset <- as.data.frame(scale(dataset, center = mins[[i]], scale = maxs[[i]] - mins[[i]]))
+	maxs[[i]] <- as.numeric(dataset[, lapply(.SD, max)])
+	names(maxs[[i]]) <- colnames(dataset)
+	mins[[i]] <- as.numeric(dataset[, lapply(.SD, min)])
+	names(mins[[i]]) <- colnames(dataset)
+
+	dataset <- as.data.table(scale(dataset, center = mins[[i]], scale = maxs[[i]] - mins[[i]]))
 
 	len.fold <- round(nrow(dataset)/folds.num)
 
-	###### Use a testing set
+	######Use a testing set
 	if(length(bases.teste) > 0 && folds.num == 1) {
 		dataset.teste <- read.csv(paste0(datasets.folder, "/", bases.teste[i], ".csv"))
-		dataset.teste <- as.data.frame(sapply(dataset.teste, function(x) as.numeric(x)))
-		dataset.teste <- as.data.frame(scale(dataset.teste, center = mins[[i]], scale = maxs[[i]] - mins[[i]]))
+
+		invisible(dataset.teste[, names(dataset.teste) := lapply(.SD, as.numeric)])
+
+		dataset.teste <- as.data.table(scale(dataset.teste, center = mins[[i]], scale = maxs[[i]] - mins[[i]]))
 		init.bound <- nrow(dataset) + 1
-		dataset <- rbind(dataset, dataset.teste)
+
+		dataset <- rbindlist(list(dataset, dataset.teste))
 		sample.names <- c(sample.names, rownames(dataset.teste))
 	}
-	rownames(dataset) <- 1:nrow(dataset)
 	#######
 
-	x <- dataset[, 1:(ncol(dataset)-length(targets[[i]]))]
-	y <- dataset[, targets[[i]]]
+	x <- dataset[, !targets[[i]], with = FALSE]
+	y <- dataset[, targets[[i]], with = FALSE]
 
 	if(showProgress){}else{print(bases[i])}
 	col.names.targets <- c()
@@ -58,30 +63,28 @@ for(i in 1:length(bases)) {
 				train.idx <- test.idx
 			}
 		} else {
-			test.idx <- as.numeric(rownames(dataset[((k-1)*len.fold + 1):(ifelse(k==folds.num, nrow(dataset), k*len.fold)),]))
-			train.idx <- as.numeric(rownames(dataset[-test.idx,]))
+			test.idx <- ((k-1)*len.fold + 1):(ifelse(k==folds.num, nrow(dataset), k*len.fold))
+			train.idx <- setdiff(1:nrow(dataset), test.idx)
 		}
 
-		x.train <- x[train.idx,]
-		y.train <- y[train.idx,]
+		x.train <- x[train.idx]
+		y.train <- y[train.idx]
 
-		x.test <- x[test.idx,]
-		y.test <- y[test.idx,]
+		x.test <- x[test.idx]
+		y.test <- y[test.idx]
 
-		prediction.log <- as.data.frame(setNames(replicate(length(col.names.targets),numeric(nrow(x.test)), simplify = F),
+		prediction.log <- as.data.table(setNames(replicate(length(col.names.targets),numeric(nrow(x.test)), simplify = F),
 													col.names.targets))
 
 		for(t in targets[[i]]) {
 			if(showProgress){pb$tick()}else{print(t)}
-			predictions <- rep(0, nrow(x.test))
-			regressor <- train_(x.train, y.train[,t], tech, targets[[i]])
+			regressor <- train_(x.train, y.train[[t]], tech, targets[[i]])
 			predictions <- predict_(regressor, x.test, tech, targets[[i]])
 
-			prediction.log[,t] <- y.test[,t]
-			prediction.log[,paste0(t, ".pred")] <- predictions
+			prediction.log[[t]] <- y.test[[t]]
+			prediction.log[[paste0(t, ".pred")]] <- predictions
 		}
-		prediction.log <- cbind(sample.names[test.idx], prediction.log)
-		write.csv(prediction.log, paste0(output.dir.st, "/prediction_logs/",tech,"/predictions_ST_", bases[i], paste0("_fold", formatC(k, width=2, flag="0")), ".csv"), row.names = FALSE)
+		write.csv(data.frame(id=sample.names[test.idx],prediction.log, check.names = F), paste0(output.dir.st, "/prediction_logs/",tech,"/predictions_ST_", bases[i], paste0("_fold", formatC(k, width=2, flag="0")), ".csv"), row.names = FALSE)
 	}
 }
 
