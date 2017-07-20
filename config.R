4c5e4957b7b5b7aa9529eb2d87c016c1350ca9cb
smastelini <saulomastelini@gmail.com>
2017-07-20 10:56:13 -0300

    ORC -> MOTC: Multi-output Tree Chaining

--- config.R
+++ config.R
@@ -1,30 +1,29 @@
 ###############################################################################
 #############################General settings##################################
 ###############################################################################
-exp.seed <- 0
+exp.seed <- 5465
 use.pls <- FALSE
 # bases <- c("atp1d","atp7d","oes97","oes10","rf1","rf2","scm1d","scm20d","edm","sf1","sf2","jura","wq","enb","slump","andro","osales","scpf")
 # n.targets <- c(6,6,16,16,8,8,16,16,2,3,3,3,14,2,3,6,12,3)
-bases <- c("NIR_Wooden_HALF12t7PC")
-n.targets <- c(12)
+
+bases <- c("scm20d")
+n.targets <- c(16)
 
 bases.teste <- NULL
 
-techs <- c("ranger","svm","cart")
+techs <- c("ranger","svm","cart","xgboost")
 
 folds.num <- 10
 
-# datasets.folder <- "~/MEGA/MT_datasets"
-# output.prefix <- "~/MEGA/Experimentos/exp_benchmarks_18dts"
-datasets.folder <- "~/Desktop"
-output.prefix <- "~/NIRSGSST"
+datasets.folder <- "~/MEGA/MT_datasets"
+output.prefix <- "~/mastelini/FULLB"
 
-mt.techs <- c("SGSST")
+mt.techs <- c("DSTARST")
 
 #Progress bar and remaining time exhibition
 showProgress <- FALSE
 
-must.compare <- TRUE
+must.compare <- FALSE
 generate.final.table <- FALSE
 ###############################################################################
 ###############################################################################
