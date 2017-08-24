892199d8561d7c2a919bb573d4c0c76c776e68f3
smastelini <saulomastelini@gmail.com>
2017-08-24 16:15:55 -0300

    R2 added into KCLUS performance log

--- global_methods/expKCLUS.R
+++ global_methods/expKCLUS.R
@@ -3,23 +3,18 @@ library(data.table)
 
 n.folds <- 10
 
-set.seed(23423)
-
 datasets.folder <- "~/MEGA/K-fold_Split"
 output.prefix <- "~/MEGA/Experimentos/exp_KCLUS/all"
-output.sufix <- "results_k=6"
+output.sufix <- "results_k=5"
 
 bases <- c("atp1d","atp7d","oes97","oes10","rf1","rf2","scm1d","scm20d","edm","sf1","sf2","jura","wq","enb","slump","andro","osales","scpf")
 n.targets <- c(6,6,16,16,8,8,16,16,2,3,3,3,14,2,3,6,12,3)
-
-# bases <- c("atp1d","atp7d","oes97","oes10","edm","sf1","sf2","jura","wq","enb","slump","andro","osales","scpf")
-# n.targets <- c(6,6,16,16,2,3,3,3,14,2,3,6,12,3)
 
 # Ensemble
 n.trees <- 100
 
 #kClus config
-ramification.factor = 6
+ramification.factor = 5
 max.depth = Inf
 var.improvp = 0.01
 min.kclus.size <- NULL
@@ -35,8 +30,13 @@ log <- data.frame(
           dataset = rep(bases, n.targets+1),
           target_index = unlist(sapply(n.targets, function(j) rep(seq(from = 0, to = j)))),
           target_name = character(sum(n.targets+1)),
-          as.data.frame(setNames(replicate(n.folds+1, numeric(sum(n.targets+1)), simplify = F),
-               c(paste0("fold", formatC(seq(n.folds), width=2, flag="0")), "mean")
+          aRRMSE = numeric(sum(n.targets+1)),
+          mean_R2 = numeric(sum(n.targets+1)),
+          as.data.frame(setNames(replicate(n.folds, numeric(sum(n.targets+1)), simplify = F),
+               paste0("RRMSE.fold", formatC(seq(n.folds), width=2, flag="0"))
+          )),
+          as.data.frame(setNames(replicate(n.folds, numeric(sum(n.targets+1)), simplify = F),
+               paste0("R2.fold", formatC(seq(n.folds), width=2, flag="0"))
           ))
        )
 
@@ -104,12 +104,15 @@ for(i in seq_along(bases)) {
     }
 
     errors <- sapply(seq(n.targets[i]), function(j, y, y.pred) RRMSE(y[[j]], y.pred[[j]]), y = y.test, y.pred = predictions)
+    set(log, init:(init + n.targets[i]), paste0("RRMSE.fold", formatC(k, width=2, flag="0")), c(mean(errors), errors))
 
-    set(log, init:(init + n.targets[i]), paste0("fold", formatC(k, width=2, flag="0")), c(mean(errors), errors))
+    rsquareds <- sapply(seq(n.targets[i]), function(j, y, y.pred) summary(lm(y[[j]] ~ y.pred[[j]]))$r.squared, y = y.test, y.pred = predictions)
+    set(log, init:(init + n.targets[i]), paste0("R2.fold", formatC(k, width=2, flag="0")), c(mean(rsquareds), rsquareds))
   }
   set(log, init:(init + n.targets[i]), "target_name", c("all", targets))
   init <- init + n.targets[i] + 1
 }
 
-log[["mean"]] <- rowMeans(log[, 5:(ncol(log)-1)])
+log[["aRRMSE"]] <- rowMeans(log[,6:15])
+log[["mean_R2"]] <- rowMeans(log[, 16:25])
 write.csv(log, paste0(output.prefix, "/", output.sufix, ".csv"), row.names = F)
