df8c3ac10aac00f61ce1665c368d7f4e778687ab
Saulo Martiello Mastelini <saulomastelini@gmail.com>
2017-11-13 14:59:18 -0200

    package bug fixes

--- global_methods/expRT.R
+++ global_methods/expRT.R
@@ -6,17 +6,14 @@ n.folds <- 10
 
 set.seed(23423)
 
-datasets.folder <- "~/MEGA/K-fold_Split"
+datasets.folder <- "~/mastelini/K-fold_Split"
 # datasets.folder <- "~/mastelini/K-fold_Split"
 output.prefix <- "~/mastelini/FKRT"
 output.sufix <- "f3rt"
 
 
-# bases <- c("atp1d","atp7d","oes97","oes10","rf1","rf2","scm1d","scm20d","edm","sf1","sf2","jura","wq","enb","slump","andro","osales","scpf")
-# n.targets <- c(6,6,16,16,8,8,16,16,2,3,3,3,14,2,3,6,12,3)
-
-bases <- c("andro")
-n.targets <- c(6)
+bases <- c("atp1d","atp7d","oes97","oes10","rf1","rf2","scm1d","scm20d","edm","sf1","sf2","jura","wq","enb","slump","andro","osales","scpf")
+n.targets <- c(6,6,16,16,8,8,16,16,2,3,3,3,14,2,3,6,12,3)
 
 # Ensemble
 
