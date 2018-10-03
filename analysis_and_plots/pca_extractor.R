e2ca8ffb30b93175c1fb2824a46bb989b59d77f0
EvertonJSantana <js.everton@hotmail.com>
2018-10-03 08:15:37 -0300

    Added the PCA transformation for a testing set
    - Added the PCA transformation for a testing set
    - Graphic of sdev with factoextra

--- analysis_and_plots/pca_extractor.R
+++ analysis_and_plots/pca_extractor.R
@@ -1,28 +1,69 @@
-dataset <- read.csv("~/MEGA/MT_datasets/atp7d.csv", stringsAsFactors = F)
+include.testing.set <-TRUE
 
-X <- dataset[, -c(412:ncol(dataset))]
+datasets.folder <- "~/Documents/mtr-fluorescencia/dataset/"
+dataset <- read.csv(paste0(datasets.folder,"data_train.csv"), stringsAsFactors = F, row.names = 1)
+
+n.targets <- 10
+
+X <- dataset[, c(1:(ncol(dataset)-n.targets))]
 X <- remove.unique(X)
-Y <- dataset[, 412:ncol(dataset)]
+Y <- dataset[, (ncol(dataset)-n.targets+1) :ncol(dataset)]
 
-source("~/mtr-toolkit/utils_and_includes/utils_MT.R")
+source("~/Documents/mtr-fluorescencia/utils_and_includes/utils_MT.R")
 
 
 # pca.m <- prcomp(X, center = TRUE, scale. = TRUE)
-# # Component choice
+# # # Component choice
 # summary(pca.m)
+# library("factoextra")
+# factoextra::fviz_screeplot(pca.m, ncp=40)
 
-num.comp <- 60
+componentes <- c(5, 10, 15)
 
-output <- matrix(nrow=nrow(dataset), ncol=num.comp, 0)
-
-for(i in 1:nrow(dataset)) {
-  pca.m <- prcomp(X[-i,], center = TRUE, scale. = TRUE)
-  output[i,] <- predict(pca.m, X[i,])[,1:num.comp]
+for (num.comp in componentes){
+  
+  output <- matrix(nrow=nrow(dataset), ncol=num.comp, 0)
+  
+  
+  for(i in 1:nrow(dataset)) {
+    pca.m <- prcomp(X[-i,], center = TRUE, scale. = TRUE)
+    output[i,] <- predict(pca.m, X[i,])[,1:num.comp]
+  }
+  
+  colnames(output) <- paste0("PC", 1:num.comp)
+  output <- as.data.frame(output)
+  
+  
+  final <- cbind(output, Y)
+  write.csv(final, paste0(datasets.folder,"PCA",num.comp,".csv"), row.names = F)
+  
 }
 
-colnames(output) <- paste0("PC", 1:num.comp)
-output <- as.data.frame(output)
 
-
-final <- cbind(output, Y)
-write.csv(final, "~/Desktop/atp7d_PCA.csv", row.names = F)
+if(include.testing.set==TRUE){
+  pca.m <- prcomp(X[,], center = TRUE, scale. = TRUE) ##new samples wouldn`t have access to new scores, so it uses the scores of the training set
+  
+  dataset <- read.csv(paste0(datasets.folder,"data_test.csv"), stringsAsFactors = F, row.names = 1)
+  
+  X <- dataset[, c(1:(ncol(dataset)-n.targets))]
+  X <- remove.unique(X)
+  Y <- dataset[, (ncol(dataset)-n.targets+1) :ncol(dataset)]
+  
+  componentes <- c(5, 10, 15)
+  
+  for (num.comp in componentes){
+    
+    output <- matrix(nrow=nrow(dataset), ncol=num.comp, 0)
+    
+    for(i in 1:nrow(dataset)) {
+      output[i,] <- predict(pca.m, X[i,])[,1:num.comp]
+    }
+    
+    colnames(output) <- paste0("PC", 1:num.comp)
+    output <- as.data.frame(output)
+    
+    final <- cbind(output, Y)
+    write.csv(final, paste0(datasets.folder,"PCA_test",num.comp,".csv"), row.names = F)
+    
+  }
+}
